2a6958f860da7720e629d18dbdbfe7f727888ca5
altpebush6 <93250409+altpebush6@users.noreply.github.com>
2022-10-23 19:11:23 +0300

    asdasd

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,24 +8,28 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		JFrame f = new JFrame("Metin2");
-		// f.setUndecorated(true); // To close top bar
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		//f.setUndecorated(true); // To close top bar
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		
 		GamePanel gamePanel = new GamePanel();
 		f.add(gamePanel);
-		f.pack(); // window will be sized to fit the preferred size and layouts of its
-					// subcomponents (GamePanel)
-
+		f.pack(); // window will be sized to fit the preferred size and layouts of its subcomponents (GamePanel) 
+		
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
-
+		
 		gamePanel.setupGame();
-
+		
 		gamePanel.startGameThread();
-
+		
 		ImageIcon favicon = new ImageIcon("resources/background/metin2.png");
 		f.setIconImage(favicon.getImage());
+<<<<<<< Updated upstream
 
 		System.out.println("amuck siktim");
+=======
+		
+		System.out.println("canı sevdim");
+>>>>>>> Stashed changes
 	}
 }
