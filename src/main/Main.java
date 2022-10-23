ee7cebe509e2638191bc4c91f4cbe87756bc41e3
CanDenizGokgedik <20050111057@ybu.edu.tr>
2022-10-23 19:08:34 +0300

    ebu

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,23 +8,24 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		JFrame f = new JFrame("Metin2");
-		//f.setUndecorated(true); // To close top bar
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-		
+		// f.setUndecorated(true); // To close top bar
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		GamePanel gamePanel = new GamePanel();
 		f.add(gamePanel);
-		f.pack(); // window will be sized to fit the preferred size and layouts of its subcomponents (GamePanel) 
-		
+		f.pack(); // window will be sized to fit the preferred size and layouts of its
+					// subcomponents (GamePanel)
+
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
-		
+
 		gamePanel.setupGame();
-		
+
 		gamePanel.startGameThread();
-		
+
 		ImageIcon favicon = new ImageIcon("resources/background/metin2.png");
 		f.setIconImage(favicon.getImage());
-		
-		System.out.println("canı siktim");
+
+		System.out.println("ebuyu siktim");
 	}
 }
