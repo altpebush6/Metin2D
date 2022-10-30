ba753122211edcfb717dff3a37abd8ddb752a0ec
Emre Özdemir <necatiiemre@gmail.com>
2022-10-30 13:59:39 +0300

    Healt and Sp specifications

--- src/main/UI.java
+++ src/main/UI.java
@@ -10,71 +10,80 @@ import javax.imageio.ImageIO;
 
 public class UI {
 
-	GamePanel gp;
-	Font arial_30;
-	BufferedImage coinImage, dolunayImage;
-	
-	public int healthBar = 192;
-	
-	public boolean messageOn = false;
-	public String message = "";
-	int messageCounter = 0;
-	public int itemIndex;
-	
-	public UI(GamePanel gp) {
-		this.gp = gp;
-	      
-		arial_30 = new Font("Arial", Font.PLAIN, 30);
-		
-		try {
-			coinImage = ImageIO.read(getClass().getResourceAsStream("/objects/yang.png"));
-			dolunayImage = ImageIO.read(getClass().getResourceAsStream("/objects/dolunayItem.png"));
-		} catch (IOException e) {}
-	}
-	
-	public void showMessage(String text) {
-		
-		message = text;
-		messageOn = true;
-	}
-	
-	public void draw(Graphics2D g2) {
-	    
-	    healthBar = gp.player.playerHealth * 2;
-	    
-	    // Health Bar
-	    g2.drawRect(100, 400, 192, 50);
-	    g2.setColor(Color.red);
-	    g2.fillRect(100, 400, healthBar, 50);
-	    
-	    // Game State yapan kişi buradaki kodları alıp gp.playState if inin içine atsın
-		
-		g2.setFont(arial_30);
-		g2.setColor(Color.white);
-		
-		// COIN
-		g2.drawImage(coinImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize,null);
-		g2.drawString(" "+gp.player.playerCoin, 60, 60);
+    GamePanel gp;
+    Font arial_30;
+    BufferedImage coinImage, dolunayImage;
 
-		switch(itemIndex) {
-			case 1:
-				g2.drawImage(dolunayImage, 335, 550, gp.tileSize/2, gp.tileSize/2,null);
-				break;
-		}
+    public int healthBar ;
+    public int spBar ;
 
-		// MESSAGE
-		if(messageOn) {
-			g2.setFont(g2.getFont().deriveFont(20F));
-			g2.drawString(message, gp.tileSize, gp.tileSize * 11);
-			
-			messageCounter++;
-			
-			if(messageCounter > 120) { // Because of FPS is 60. 120 means 2 seconds
-				messageCounter = 0;
-				messageOn = false;
-			}
-			
-		}
-	}
-	
+    public boolean messageOn = false;
+    public String message = "";
+    int messageCounter = 0;
+    public int itemIndex;
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+
+        arial_30 = new Font("Arial", Font.PLAIN, 30);
+
+        try {
+            coinImage = ImageIO.read(getClass().getResourceAsStream("/objects/yang.png"));
+            dolunayImage = ImageIO.read(getClass().getResourceAsStream("/objects/dolunayItem.png"));
+        } catch (IOException e) {
+        }
+    }
+
+    public void showMessage(String text) {
+
+        message = text;
+        messageOn = true;
+    }
+
+    public void draw(Graphics2D g2) {
+
+        healthBar = gp.player.playerHealth * 2;
+        spBar = gp.player.playerSp * 2;
+
+        // Health Bar
+        g2.drawRoundRect(gp.tileSize / 3, gp.tileSize * (gp.maxScreenRow - 1), 201, 15, 20, 20);
+        g2.setColor(Color.red);
+        g2.fillRoundRect(gp.tileSize / 3, gp.tileSize * (gp.maxScreenRow - 1), healthBar, 15, 20, 20);
+
+        // Sp Bar
+        g2.setColor(Color.black);
+        g2.drawRoundRect(gp.tileSize / 3, gp.tileSize * (gp.maxScreenRow -1) + (gp.tileSize/2), 201, 15, 20, 20);
+        g2.setColor(Color.blue);
+        g2.fillRoundRect(gp.tileSize / 3, gp.tileSize * (gp.maxScreenRow -1) +(gp.tileSize/2), spBar, 15, 20, 20);
+
+        // Game State yapan kişi buradaki kodları alıp gp.playState if inin içine atsın
+
+        g2.setFont(arial_30);
+        g2.setColor(Color.white);
+
+        // COIN
+        g2.drawImage(coinImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
+        g2.drawString(" " + gp.player.playerCoin, 60, 60);
+
+        switch (itemIndex) {
+            case 1:
+                g2.drawImage(dolunayImage, 335, 550, gp.tileSize / 2, gp.tileSize / 2, null);
+                break;
+        }
+
+        // MESSAGE
+        if (messageOn) {
+            g2.setFont(g2.getFont().deriveFont(20F));
+            g2.drawString(message, gp.tileSize, gp.tileSize * 11);
+
+            messageCounter++;
+
+            if (messageCounter > 120) { // Because of FPS is 60. 120 means 2 seconds
+                messageCounter = 0;
+                messageOn = false;
+            }
+
+        }
+    }
+
 }
