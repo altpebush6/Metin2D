4cfad00940a67637b76174caff89ad318aa28c94
CanDenizGokgedik <20050111057@ybu.edu.tr>
2022-11-27 20:23:43 +0300

    c

--- src/enemy/ENEMY_Wolf.java
+++ src/enemy/ENEMY_Wolf.java
@@ -4,18 +4,20 @@ import java.util.Random;
 
 import entity.Entity;
 import main.GamePanel;
+import ai.Node;
+import ai.PathFinder;
 
 public class ENEMY_Wolf extends Entity {
 
     Random rand = new Random();
     GamePanel gp;
-        
+
     public ENEMY_Wolf(GamePanel gp, int id) {
-        
+
         super(gp);
-        
+
         this.gp = gp;
-        
+
         name = "Wolf";
         wolfID = id;
         speed = 1;
@@ -23,34 +25,42 @@ public class ENEMY_Wolf extends Entity {
         maxLife = 30;
         life = maxLife;
         type = 1;
-        standing = true;        
-        
+        standing = true;
+
         int bornDirection = rand.nextInt(4) + 1;
-        switch(bornDirection) {
-            case 1: direction = "up";       break;
-            case 2: direction = "down";     break;
-            case 3: direction = "left";     break;
-            case 4: direction = "right";    break;
+        switch (bornDirection) {
+            case 1:
+                direction = "up";
+                break;
+            case 2:
+                direction = "down";
+                break;
+            case 3:
+                direction = "left";
+                break;
+            case 4:
+                direction = "right";
+                break;
         }
-        
-        if(direction == "up" || direction == "down") {
+
+        if (direction == "up" || direction == "down") {
             solidArea.x = 10;
             solidArea.y = 0;
             solidArea.width = 25;
             solidArea.height = 48;
-        }else {
+        } else {
             solidArea.x = 0;
             solidArea.y = 8;
             solidArea.width = 48;
             solidArea.height = 32;
         }
-        
+
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
-        
+
         getImage();
     }
-    
+
     public void getImage() {
         up1 = setup("/wolf/up1", gp.tileSize, gp.tileSize);
         up2 = setup("/wolf/up2", gp.tileSize, gp.tileSize);
@@ -62,81 +72,97 @@ public class ENEMY_Wolf extends Entity {
         left2 = setup("/wolf/left2", gp.tileSize, gp.tileSize);
         left3 = setup("/wolf/left3", gp.tileSize, gp.tileSize);
         right1 = setup("/wolf/right1", gp.tileSize, gp.tileSize);
-        right2 = setup("/wolf/right2", gp.tileSize, gp.tileSize); 
+        right2 = setup("/wolf/right2", gp.tileSize, gp.tileSize);
         right3 = setup("/wolf/right3", gp.tileSize, gp.tileSize);
     }
-    
+
     public void update() {
-        
+
         super.update();
-        
+
         int xDistance = Math.abs(worldX - gp.player.worldX);
         int yDistance = Math.abs(worldY - gp.player.worldY);
         int tileDistance = (xDistance + yDistance) / gp.tileSize;
-        
-        if(onPath && tileDistance > 10) {
+
+        if (onPath && tileDistance > 10) {
             onPath = false;
         }
     }
-    
+
     public void setAction() {
-        
-        if(onPath) {
+
+        if (onPath) {
             standing = false;
-            
+
             speed = 2;
-            
+
             int goalCol = (gp.player.worldX) / gp.tileSize; // gp.player.worldX + gp.player.solidArea.x
             int goalRow = (gp.player.worldY) / gp.tileSize; // gp.player.worldY + gp.player.solidArea.y
-            
+
             searchPath(goalCol, goalRow);
-        }else {
+        } else {
             speed = defaultSpeed;
             actionLockCounter++;
-            
+
             int firstChange = 60;
             int increaseAmount = 180;
-            
+
             // 1 seconds after born, change direction
-            if(actionLockCounter == firstChange) {
+            if (actionLockCounter == firstChange) {
                 standing = false;
                 changeDirection();
             }
-            
+
             // 3 seconds after first change direction, stop
-            if(actionLockCounter == (firstChange + increaseAmount)) {
+            if (actionLockCounter == (firstChange + increaseAmount)) {
                 standing = true;
-            }       
-            
+            }
+
             // 3 seconds after, change direction again
-            if(actionLockCounter == (firstChange + increaseAmount * 2)) {
+            if (actionLockCounter == (firstChange + increaseAmount * 2)) {
                 standing = false;
                 changeDirection();
             }
-            
+
             // stop for 3 seconds and enter to the second if
-            if(actionLockCounter == (firstChange + increaseAmount * 3)) {
+            if (actionLockCounter == (firstChange + increaseAmount * 3)) {
                 standing = true;
                 actionLockCounter = firstChange;
             }
         }
     }
-    
-    public void changeDirection(){
+
+    public void changeDirection() {
         int nextMove = rand.nextInt(8) + 1;
-        
-        switch(nextMove) {
-            case 1: direction = "up";       break;
-            case 2: direction = "upleft";   break;
-            case 3: direction = "upright";  break;
-            case 4: direction = "down";     break;
-            case 5: direction = "downleft"; break;
-            case 6: direction = "downright";break;
-            case 7: direction = "left";     break;
-            case 8: direction = "right";    break;
+
+        switch (nextMove) {
+            case 1:
+                direction = "up";
+                break;
+            case 2:
+                direction = "upleft";
+                break;
+            case 3:
+                direction = "upright";
+                break;
+            case 4:
+                direction = "down";
+                break;
+            case 5:
+                direction = "downleft";
+                break;
+            case 6:
+                direction = "downright";
+                break;
+            case 7:
+                direction = "left";
+                break;
+            case 8:
+                direction = "right";
+                break;
         }
     }
-    
+
     public void damageReaction() {
         actionLockCounter = 0;
         onPath = true;
