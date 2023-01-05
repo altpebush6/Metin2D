b2c1ee55533ec33f442e01355cf069db7e65ff8b
biehcey <116308507+biehcey@users.noreply.github.com>
2023-01-05 23:51:33 +0300

    javadoc.1

--- src/data/DataStorage.java
+++ src/data/DataStorage.java
@@ -12,24 +12,88 @@ import entity.Entity;
  */
 public class DataStorage implements Serializable {
     
-    /**
-     * <p>
-     * These fields are created to store the stats of the character 
-     * </p>
-     */
+    
     // PLAYER STATS
-    int level, playerCoin, playerXP, attackPower, redPotionNumber, bluePotionNumber, taskLevel;
-    String playerWeapon, playerName;
     /**
      * <p>
-     * These fields are created to store the items of the character 
+     * This field stores characters level
      * </p>
      */
+    public int level;
+    /**
+     * <p>
+     * This field stores characters coin
+     * </p>
+     */
+    public int playerCoin;
+    /**
+     * <p>
+     * This field stores characters experience point
+     * </p>
+     */
+    public int playerXP; 
+    /**
+     * <p>
+     * This field stores characters attack power
+     * </p>
+     */
+    public int attackPower;
+    /**
+     * <p>
+     * This field stores how many red potion character have
+     * </p>
+     */
+    public int redPotionNumber;
+    /**
+     * <p>
+     * This field stores how many blue potions character have
+     * </p>
+     */
+    public int bluePotionNumber;
+    /**
+     * <p>
+     * This field stores characters task level
+     * </p>
+     */
+    public int taskLevel;
+    /**
+     * <p>
+     * This field stores characters weapon name
+     * </p>
+     */
+    public String playerWeapon;
+    /**
+     * <p>
+     * This field stores characters name
+     * </p>
+     */
+    public String playerName;
+    
     //PLAYER INVENTORY
+    /**
+     * <p>
+     * This field stores item names in the inventory
+     * </p>
+     */
     ArrayList<String> itemNames = new ArrayList<>();
+    /**
+     * <p>
+     * This field stores items level
+     * </p>
+     */
     ArrayList<Integer> enhancedLevel = new ArrayList<>();
+    /**
+     * <p>
+     * This field stores characters current weapon's name
+     * </p>
+     */
     String currentWeaponName;
     
+    /**
+     * <p>
+     * This stores to whether mission is taken or not
+     * </p>
+     */
     int[] missionPrize = new int[5];
   
 }
